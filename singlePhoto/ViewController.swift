ac8ec61e7819e47e6fae93463536e95411859e39
GabrielCee27 <gabrielcee81@gmail.com>
2017-10-20 20:07:18 -0700

    Initial Commit

---
+++ singlePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  singlePhoto
+//
+//  Created by Gabriel Cisneros on 10/20/17.
+//  Copyright © 2017 Gabriel Cisneros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
